daee08dfc5bcc30cefa24d89882e7287b8c04375
AMIT VERMA <amit.verma@softobotics.com>
2024-11-09 17:11:39 +0530

    sda

--- src/app/theme/layouts/admin-layout/nav-bar/nav-bar.component.ts
+++ src/app/theme/layouts/admin-layout/nav-bar/nav-bar.component.ts
@@ -38,14 +38,14 @@ export class NavBarComponent {
   // public method
   navCollapse() {
     // if (this.windowWidth >= 1025) {
-      this.navCollapsed = !this.navCollapsed;
-      this.NavCollapse.emit();
+      // this.navCollapsed = !this.navCollapsed;
+      // this.NavCollapse.emit();
     // }
   }
 
   navCollapseMob() {
     // if (this.windowWidth < 1025) {
-      this.NavCollapsedMob.emit();
+      // this.NavCollapsedMob.emit();
     // }
   }
 }
